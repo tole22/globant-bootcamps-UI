fc870f8acb8a72507785a0e03e3d2ad227d9cd68
tole22 <tole.maxi@hotmail.com>
2018-08-15 15:42:05 -0300

    Tour of Heores angular tutorial completedgit add -Agit add -A

--- src/week3/angular-tour-of-heroes/src/app/dashboard/dashboard.component.ts
+++ src/week3/angular-tour-of-heroes/src/app/dashboard/dashboard.component.ts
@@ -5,7 +5,8 @@ import { HeroService } from '../hero.service';
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
-  styleUrls: [ './dashboard.component.css' ]
+  styleUrls: [ './dashboard.component.css' ],
+  providers: [HeroService]
 })
 export class DashboardComponent implements OnInit {
   heroes: Hero[] = [];
--- src/week3/angular-tour-of-heroes/src/app/hero-detail/hero-detail.component.ts
+++ src/week3/angular-tour-of-heroes/src/app/hero-detail/hero-detail.component.ts
@@ -1,25 +1,26 @@
 import { Component, OnInit, Input } from '@angular/core';
-import { Hero } from '../hero';
-
 import { ActivatedRoute } from '@angular/router';
 import { Location } from '@angular/common';
 
+import { Hero }         from '../hero';
 import { HeroService }  from '../hero.service';
 
 @Component({
   selector: 'app-hero-detail',
   templateUrl: './hero-detail.component.html',
-  styleUrls: ['./hero-detail.component.css']
+  styleUrls: [ './hero-detail.component.css' ],
+  providers: [HeroService]
 })
 export class HeroDetailComponent implements OnInit {
-
   @Input() hero: Hero;
 
-  constructor(private route: ActivatedRoute,
+  constructor(
+    private route: ActivatedRoute,
     private heroService: HeroService,
-    private location: Location) { }
+    private location: Location
+  ) {}
 
-  ngOnInit() {
+  ngOnInit(): void {
     this.getHero();
   }
 
@@ -33,4 +34,8 @@ export class HeroDetailComponent implements OnInit {
     this.location.back();
   }
 
+ save(): void {
+    this.heroService.updateHero(this.hero)
+      .subscribe(() => this.goBack());
+  }
 }
