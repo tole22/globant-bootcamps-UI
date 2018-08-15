4333751191c34aa2caebcbdf4235d162f80e5d27
tole22 <tole.maxi@hotmail.com>
2018-08-15 11:11:45 -0300

    Tour of Heores angular steps 4 5 6

--- src/week3/angular-tour-of-heroes/src/app/heroes/heroes.component.ts
+++ src/week3/angular-tour-of-heroes/src/app/heroes/heroes.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
+import { HEROES } from '../mock-heroes';
+import { HeroService } from '../hero.service';
 
 @Component({
   selector: 'app-heroes',
@@ -8,14 +10,22 @@ import { Hero } from '../hero';
 })
 export class HeroesComponent implements OnInit {
 
-  hero: Hero = {
-    id: 1,
-    name: 'Windstorm'
-  };
+  heroes: Hero[];
 
-  constructor() { }
+  selectedHero: Hero;
+
+  constructor(private heroService: HeroService) { }
 
   ngOnInit() {
+    this.getHeroes();
   }
 
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
+
+  getHeroes(): void {
+    this.heroService.getHeroes()
+        .subscribe(heroes => this.heroes = heroes);
+  }
 }
